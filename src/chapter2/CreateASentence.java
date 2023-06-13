ca3a0cf308faf641c4b73c2815b063b1a159d540
Balázs <kelecsenyibusiness@gmail.com>
2023-06-13 20:45:57 +0200

    Homework

--- src/chapter2/CreateASentence.java
+++ src/chapter2/CreateASentence.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class CreateASentence {
 
     public static void main(String arg[]){
+        // Create a sentences with the user answers
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please give a season of the year: ");
         String seasonOfYear = scanner.next();
