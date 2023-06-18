c6961b6ba3de38f44acc05a630195288fb541916
Balázs <kelecsenyibusiness@gmail.com>
2023-06-18 11:45:54 +0200

    HomeCalculator

---
+++ src/chapter6/HomeAreaCalculatorRedo.java
@@ -0,0 +1,26 @@
+package chapter6;
+import java.util.Scanner;
+
+public class HomeAreaCalculatorRedo {
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String args[]){
+        HomeAreaCalculatorRedo calculator = new HomeAreaCalculatorRedo();
+        Rectangle kitchen = calculator.getRoom();
+        Rectangle bathroom = calculator.getRoom();
+        double area = calculator.calculatorTotalArea(kitchen, bathroom);
+        System.out.println("Total area: "+area);
+        calculator.scanner.close();
+
+    }
+    public Rectangle getRoom(){
+        System.out.println("Enter the length");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width");
+        double width = scanner.nextDouble();
+        return new Rectangle(length,width);
+    }
+
+    public double calculatorTotalArea(Rectangle rectangle1, Rectangle rectangle2){
+        return rectangle1.calculateArea() + rectangle2.calculateArea();
+    }
+}
