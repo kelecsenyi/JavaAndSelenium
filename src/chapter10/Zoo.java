00dbf2a346cac6683d9efc0a12e2db91f13cabcb
Balázs <kelecsenyibusiness@gmail.com>
2023-06-21 21:44:58 +0200

    abstract and interfaces

---
+++ src/chapter10/Zoo.java
@@ -0,0 +1,15 @@
+package chapter10;
+
+public class Zoo {
+    public static void main(String args[]){
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
+
+        Animal sasha = new Dog();
+        sasha.makeSound();
+        sasha = new Cat();
+        sasha.makeSound();
+       // ((Cat) sasha).scratch();
+    }
+}
--- src/chapter9/InheritanceTester.java
+++ src/chapter9/InheritanceTester.java
@@ -3,7 +3,10 @@ package chapter9;
 public class InheritanceTester {
     public static void main(String args[]){
        // Person person = new Person();
-        Employee employee = new Employee();
+        //Employee employee = new Employee();
+        Mother mom = new Mother();
+       // mom.setName("Glenda");
+       // System.out.println(mom.getName() + "is a "+ mom.getGender());
 
     }
 }
